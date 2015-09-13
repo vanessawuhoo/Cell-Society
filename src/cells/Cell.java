9447807af7fc5a2400777f11db8af403018c3b8d
Christopher Hong <6marc6100@gmail.com>
2015-09-13 15:07:41 -0400

    Updated Cell class
    Added general methods for cell

--- src/cells/Cell.java
+++ src/cells/Cell.java
@@ -1,5 +1,31 @@
 package cells;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
 
+	private List<Double> state;
+	private List<Cell> connections;
+	
+	public List<Double> getState() {
+		return state;
+	}
+
+	public void setState(List<Double> state) {
+		this.state = state;
+	}
+
+	public List<Cell> getConnections() {
+		return connections;
+	}
+
+	public void setConnections(List<Cell> connections) {
+		this.connections = connections;
+	}
+	
+	public Cell(List<Double> s) {
+		state = s;
+		connections = new ArrayList<Cell>();
+	}
 }
