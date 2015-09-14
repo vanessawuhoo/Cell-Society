e89911205a859ae41ba8350dc817c3883ea9bc86
Kevin Wang <kevin.kaiwen.wang@gmail.com>
2015-09-14 18:29:17 -0400

    basic XML data template in DATA folder

--- src/data/XMLLoader.java
+++ src/data/XMLLoader.java
@@ -1,14 +1,19 @@
 package data;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.util.Map;
-
 import cells.Cell;
 
 public class XMLLoader {
 	private String simulationType;
 	private Map<String, String> parameters;
-	private Map<String, Map<String, String>> cells;
+	private Map<Integer, Map<String, String>> cells;
 	
 	/*
 	 * load xml 
