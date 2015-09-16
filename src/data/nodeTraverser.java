613151d5a288b0cc9a07135fefd92cba2825c366
Kevin Wang <kevin.kaiwen.wang@gmail.com>
2015-09-16 15:23:39 -0400

    nodetraverser super class retry

---
+++ src/data/nodeTraverser.java
@@ -0,0 +1,32 @@
+package data;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class nodeTraverser {
+	protected Node getNode(String tagName, NodeList nodes) {
+	    for ( int x = 0; x < nodes.getLength(); x++ ) {
+	        Node node = nodes.item(x);
+	        if (node.getNodeName().equalsIgnoreCase(tagName)) {
+	            return node;
+	        }
+	    }
+	 
+	    return null;
+	}
+	 
+	protected String getNodeValue(String tagName, NodeList nodes ) {
+	    for ( int x = 0; x < nodes.getLength(); x++ ) {
+	        Node node = nodes.item(x);
+	        if (node.getNodeName().equalsIgnoreCase(tagName)) {
+	            NodeList childNodes = node.getChildNodes();
+	            for (int y = 0; y < childNodes.getLength(); y++ ) {
+	                Node data = childNodes.item(y);
+	                if ( data.getNodeType() == Node.TEXT_NODE )
+	                    return data.getNodeValue();
+	            }
+	        }
+	    }
+	    return "";
+	}
+}
