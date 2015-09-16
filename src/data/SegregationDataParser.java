d019d6118df8b34629e906f7f1e4513d55f19573
Kevin Wang <kevin.kaiwen.wang@gmail.com>
2015-09-16 02:11:59 -0400

    added segregationdataparser, loader constructor and file load

---
+++ src/data/SegregationDataParser.java
@@ -0,0 +1,11 @@
+package data;
+
+public class SegregationDataParser extends DataParser {
+
+	@Override
+	public void parseData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/data/XMLLoader.java
+++ src/data/XMLLoader.java
@@ -1,25 +1,66 @@
 package data;
 
+import javax.xml.parsers.DocumentBuilder; 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException; 
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 import java.util.Map;
+
 import cells.Cell;
+
 
 public class XMLLoader {
 	private String simulationType;
 	private Map<String, String> parameters;
 	private Map<Integer, Map<String, String>> cells;
+	private String fileName;
+	private DocumentBuilderFactory dbf;
+	DocumentBuilder db;
+	private List<DataParser> parserList;
 	
+	public XMLLoader(){
+		dbf = DocumentBuilderFactory.newInstance();
+		try {
+			db = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	public void setFileName(String name){
+		fileName = name;
+	}
 	/*
 	 * load xml 
 	 */
 	public void load(){
-		
+		try {
+			Document doc = db.parse(new File(fileName));
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	};
 	
 	/*
@@ -28,10 +69,5 @@ public class XMLLoader {
 	public void parse(){
 		
 	}
-
-	public Object[] getData(String xml_file_name) {
-		// TODO Auto-generated method stub
-		return null;
-	};
 
 }
