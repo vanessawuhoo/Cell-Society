e54f297cbe8e2d772c3869f7fb7eda5c56d5a560
Kevin Wang <kevin.kaiwen.wang@gmail.com>
2015-09-18 17:13:17 -0400

    added parameter to wrapper

--- src/data/AllData.java
+++ src/data/AllData.java
@@ -1,14 +1,18 @@
 package data;
 
+import java.util.Map;
+
 import cells.CellGraph;
 import simulation_type.Rule;
 
 public class AllData {
 	public Rule rule;
 	public CellGraph cellGraph;
+	public Map<String, Double> parameter;
 
-	public AllData(Rule r, CellGraph cg){
+	public AllData(Rule r, CellGraph cg, Map<String, Double> p){
 		rule = r;
 		cellGraph = cg;
+		parameter = p;
 	}
 }
