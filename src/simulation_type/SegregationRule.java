d1677bb429a20921ad0c1e3172163d620e14612f
Christopher Hong <6marc6100@gmail.com>
2015-09-17 21:35:39 -0400

    Change of states
    Made state type Map<String, Double>

--- src/simulation_type/SegregationRule.java
+++ src/simulation_type/SegregationRule.java
@@ -1,12 +1,12 @@
 package simulation_type;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class SegregationRule extends Rule {
-
 	public SegregationRule(int dim_x, int dim_y) {
 		super.setGrid_type("rectangle");
 		super.setGrid_parameters(new int[] {dim_x, dim_y});
@@ -21,21 +21,24 @@ public class SegregationRule extends Rule {
 	}
 	
 	/*
-	 * States: 0=empty, 1=X, 2=O
+	 * States: 
+	 * "state": 0=empty, 1=X, 2=O
 	 */
 	@Override
-	public void updateCell(int id, List<Double> cell_state, List<List<Double>> neighboring_states,
-			Map<Integer, List<Double>> current_states, Map<Integer, List<Double>> next_states) {
+	public void updateCell(int id, Map<String, Double> cell_state, 
+			List<Map<String, Double>> neighboring_states,
+			Map<Integer, Map<String, Double>> current_states, 
+			Map<Integer, Map<String, Double>> next_states) {
 		if (next_states.containsKey(id))
 			return;
-		double state = cell_state.get(0);
+		double state = cell_state.get("state");
 		if (state == 0)
 			return;
 		int total_states = 0;
 		int opposite_states = 0;
-		for (List<Double> neighbor_state: neighboring_states) {
+		for (Map<String, Double> neighbor_state: neighboring_states) {
 			total_states++;
-			double n_state = neighbor_state.get(0);
+			double n_state = neighbor_state.get("state");
 			if (n_state != 0) {
 				if (state != n_state)
 					opposite_states++;
@@ -44,23 +47,30 @@ public class SegregationRule extends Rule {
 		double opposite_ratio = opposite_states/total_states;
 		if (opposite_ratio >= 0.5) {
 			for (int id_curr: current_states.keySet()) {
-				List<Double> curr_state = current_states.get(id_curr);
-				if (curr_state.get(0) == 0) {
-					if (!next_states.containsKey(id_curr))
-						next_states.put(id_curr, cell_state);
+				double curr_state = current_states.get(id_curr).get("state");
+				if (curr_state == 0) {
+					if (!next_states.containsKey(id_curr)) {
+						next_states.put(id_curr, makeNewState(curr_state));
+					}
 				}
 			}
 		} else {
-			next_states.put(id, cell_state);
+			next_states.put(id, makeNewState(state));
 		}
 	}
 	
+	private Map<String, Double> makeNewState(double s){
+		Map<String, Double> new_state = new HashMap<String, Double>();
+		new_state.put("state", s);
+		return new_state;
+	}
+	
 	@Override
-	public void fillVoids(Set<Integer> ids, Map<Integer, List<Double>> next_states) {
+	public void fillVoids(Set<Integer> ids, Map<Integer, Map<String, Double>> next_states) {
 		for (int id: ids) {
 			if (!next_states.containsKey(id)) {
-				ArrayList<Double> zero = new ArrayList<Double>();
-				zero.add((double) 0);
+				Map<String, Double> zero = new HashMap<String, Double>();
+				zero.put("state", 0.0);
 				next_states.put(id, zero);
 			}
 		}
